4c33991543a17113c73b2ef0e4ed913effebf308
Ilya Andreev <ilyaa1982@gmail.com>
2020-06-16 23:18:08 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+const Keyboard = {
+  elements: {
+    main: null,
+    keysContainer: null,
+    keys: [],
+  },
+
+  eventHandlers: {
+    onInput: null,
+    onClose: null,
+  },
+
+  properties: {
+    value: "",
+    capsLock: false,
+  },
+
+  init() {
+    this.elements.main = document.createElement("div");
+    this.elements.keysContainer = document.createElement("div");
+
+    this.elements.main.classList.add("keyboard", "1keyboard--hidden");
+    this.elements.keysContainer.classList.add("keyboard__keys");
+    this.elements.keysContainer.appendChild(this._createKeys());
+
+    this.elements.keys = this.elements.keysContainer.querySelectorAll(
+      ".keyboard__key"
+    );
+
+    this.elements.main.appendChild(this.elements.keysContainer);
+    document.body.appendChild(this.elements.main);
+
+    document.querySelectorAll(".use-keyboard-input").forEach((element) => {
+      element.addEventListener("focus", () => {
+        this.open(element.value, (currentValue) => {
+          element.value = currentValue;
+        });
+      });
+    });
+  },
+
+  createKeys() {
+    const fragment = document.createDocumentFragment();
+    const keyLayout = ["1", "2"];
+  },
+};
+
+window.addEventListener("DOMContentLoaded", function () {
+  Keyboard.init();
+});
